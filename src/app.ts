16e778c2f18912bdd8479c2ea6724dfecc1df5c3
Springhawk27 <sajjad0mahmud@gmail.com>
2024-11-24 00:04:16 +0600

    eslint and prettier implemented

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 // src/app.ts
-import express, { Application } from "express";
+import express, { Application } from 'express';
 const app: Application = express();
-const port = 3000;
+// const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
 export default app;
