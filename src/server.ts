e50f6cbe42c0dd0348f393780ced96e1e3e430db
Springhawk27 <sajjad0mahmud@gmail.com>
2024-11-23 23:56:36 +0600

    database connection implemented

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,13 @@ import config from "./config/index";
 
 async function bootstrap() {
   try {
+    await mongoose.connect(config.database_url as string);
+    console.log(`Database Connected Successfully`);
     app.listen(config.port, () => {
       console.log(`Application listening on port ${config.port}`);
     });
   } catch (err) {
-    console.log(`Connection Failed`, err);
+    console.log(`Database Connection Failed`, err);
   }
 }
 
