18faeb8c322c6d94927adbbc5aab7a5739645229
Springhawk27 <sajjad0mahmud@gmail.com>
2024-11-24 00:44:41 +0600

    product service implemented

---
+++ src/app/modules/product/product.service.ts
@@ -0,0 +1,14 @@
+import { TProduct } from './product.interface';
+import { Product } from './product.model';
+
+const createProductIntoDB = async (productData: TProduct) => {
+  if (await Product.isProductExists(productData.id)) {
+    throw new Error('Product already exists!');
+  }
+  const result = await Product.create(productData);
+  return result;
+};
+
+export const ProductServices = {
+  createProductIntoDB,
+};
