7ccdddcc3ae4f178cdff0ef22a80279bf3b8fd37
Springhawk27 <sajjad0mahmud@gmail.com>
2024-11-24 00:40:11 +0600

    product controller with gereric error response implemented

---
+++ src/app/modules/product/product.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+
+const createProduct = async (req: Request, res: Response) => {
+  try {
+    const { product: productData } = req.body;
+
+    const result = await ProductServices.createProductIntoDB(productData);
+
+    res.status(200).json({
+      success: true,
+      message: 'Product is created succesfully',
+      data: result,
+    });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message || 'Something went wrong',
+      error: err,
+    });
+  }
+};
+
+export const ProductControllers = {
+  createProduct,
+};
